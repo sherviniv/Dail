b5a9d6581e280d5f638d924b80f4ba8198500803
Shervin <sherviniv@gmail.com>
2022-01-16 19:40:39 +0330

    Add Dail Exception

---
+++ src/Dail.Application/Common/Exceptions/DailException.cs
@@ -0,0 +1,37 @@
+using System.Net;
+
+namespace Dail.Application.Common.Exceptions;
+
+public class DailException : Exception
+{
+    public string Code { get; }
+    public HttpStatusCode StatusCode { get; set; }
+    public DailException()
+    {
+    }
+
+    public DailException(string code)
+    {
+        Code = code;
+    }
+
+    public DailException(string message, HttpStatusCode httpStatusCode, params object[] args) : this(string.Empty, message, httpStatusCode, args)
+    {
+    }
+
+    public DailException(string code, string message, HttpStatusCode httpStatusCode = HttpStatusCode.BadRequest, params object[] args) : this(null, code, message, args)
+    {
+        StatusCode = httpStatusCode;
+    }
+
+    public DailException(Exception innerException, string message, params object[] args)
+        : this(innerException, string.Empty, message, args)
+    {
+    }
+
+    public DailException(Exception innerException, string code, string message, params object[] args)
+        : base(string.Format(message, args), innerException)
+    {
+        Code = code;
+    }
+}
