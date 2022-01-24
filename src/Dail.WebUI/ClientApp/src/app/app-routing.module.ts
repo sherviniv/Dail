00969aacee703b5cbec19a8b248cd6611ac706ff
Shervin <sherviniv@gmail.com>
2022-01-25 00:33:54 +0330

    Add not found page,fix routings

--- src/Dail.WebUI/ClientApp/src/app/app-routing.module.ts
+++ src/Dail.WebUI/ClientApp/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { Roles } from './core/models';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     loadChildren: () => import('./pages/authentication/authentication.module').then(mod => mod.AuthenticationModule)
   },
   {
@@ -17,6 +19,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     data: { roles: [Roles.DailUser] }
   },
+  { path: 'not-found', component: NotFoundComponent, pathMatch: 'full', data: { title: '404 not found' } },
   {
     path: '**',
     redirectTo: 'not-found'
--- src/Dail.WebUI/ClientApp/src/app/pages/authentication/authentication-routing.module.ts
+++ src/Dail.WebUI/ClientApp/src/app/pages/authentication/authentication-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
+    { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent }
 ];
