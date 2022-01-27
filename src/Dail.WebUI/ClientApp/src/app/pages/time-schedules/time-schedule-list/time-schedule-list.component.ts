6b16fbb0a60d214f65f96d0f0a4a3c3dbe58e292
Shervin <sherviniv@gmail.com>
2022-01-27 20:09:48 +0330

    Add datatable, time schedule list page

--- src/Dail.WebUI/ClientApp/src/app/pages/time-schedules/time-schedule-list/time-schedule-list.component.ts
+++ src/Dail.WebUI/ClientApp/src/app/pages/time-schedules/time-schedule-list/time-schedule-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { TimeSchedulesClient } from 'src/app/core/services/dail.service';
+import { ColumnMode, SortType, SelectionType } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-time-schedule-list',
@@ -6,10 +10,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./time-schedule-list.component.scss']
 })
 export class TimeScheduleListComponent implements OnInit {
+  ColumnMode = ColumnMode;
+  SelectionType = SelectionType
+  selected = [];
 
-  constructor() { }
+  rows = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' }
+  ];
+  columns = [{ prop: 'name', name: 'نام' }, { name: 'Gender' }, { name: 'Company' }];
+
+  constructor(
+    private client: TimeSchedulesClient,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+  }
+
+  async loadTable() {
+    this.spinner.show('main');
+    await this.client.getTimeScheduleList().toPromise().then(
+      response => {
+      },
+      error =>
+        this.toastr.error("خطا در ثبت اطلاعات")
+    );
+    this.spinner.hide('main');
+  }
+
+  onSelect({ selected } : any) {
+    console.log('Select Event', this.selected[0]);
   }
 
 }
--- src/Dail.WebUI/ClientApp/src/app/pages/time-schedules/time-schedules.module.ts
+++ src/Dail.WebUI/ClientApp/src/app/pages/time-schedules/time-schedules.module.ts
@@ -3,14 +3,20 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CoreModule } from 'src/app/core/core.module';
 import { TimeSchedulesRoutingModule } from './time-schedules-routing.module';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { TimeScheduleFormComponent } from './time-schedule-form/time-schedule-form.component';
+import { TimeScheduleListComponent } from './time-schedule-list/time-schedule-list.component';
 
 @NgModule({
   declarations: [
+    TimeScheduleListComponent,
+    TimeScheduleFormComponent
   ],
   imports: [
     CoreModule,
     CommonModule,
     FormsModule,
+    NgxDatatableModule,
     TimeSchedulesRoutingModule
   ]
 })
